aa4baae04f1a9cb3abe034d0e9d41a0ff436614f
guigocae <117747661+guigocae@users.noreply.github.com>
2025-04-20 08:04:41 -0300

    add animations and merge webpack config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './assets/css/index.css';
 import './assets/css/menu.css';
 import './assets/css/about.css';
+import 'animate.css';
 import Home from './pages/home';
 import Menu from './pages/menu';
 import About from './pages/about';
@@ -13,9 +14,20 @@ Home(content);
     const menu = document.querySelector("#menu");
     const about = document.querySelector("#about");
 
-    home.addEventListener("click", () => Home(content));
-    menu.addEventListener("click", () => Menu(content));
-    about.addEventListener("click", () => About(content));
+    home.addEventListener("click", () => {
+        Home(content);
+        document.querySelector("h1").classList.add('animate__animated', 'animate__fadeIn');
+    });
+    menu.addEventListener("click", () => {
+        Menu(content);
+        document.querySelector("h1").classList.add('animate__animated', 'animate__fadeIn');
+        document.querySelector(".items").classList.add('animate__animated', 'animate__fadeIn');
+    });
+    about.addEventListener("click", () => {
+        About(content);
+        document.querySelector("h1").classList.add('animate__animated', 'animate__fadeIn');
+        document.querySelector("#about-content").classList.add('animate__animated', 'animate__fadeIn');
+    });
     
     console.log(home);
 })();
--- webpack.common.js
+++ webpack.common.js
@@ -3,16 +3,11 @@ const { type } = require("os");
 const path = require("path");
 
 module.exports = {
-    mode: "development",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/template.html"],
     },
     plugins: [
         new HtmlWebpackPlugin({
